e727c04c83d777ff657b292a1958e9722b4c7e69
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-05-17 18:48:05 +0500

    Add Header Image

---
+++ src/components/City/City.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function City() {
+  return (
+    <div className="city">
+      <h4>Lahore</h4>
+      <h6>City Temperature</h6>
+      <h6>CPI Index</h6>
+    </div>
+  );
+}
+
+export default City;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import City from '../../components/City/City';
+import './style.css';
+
+export default function Home() {
+  return (
+    <div className="home">
+      <City />
+    </div>
+  );
+}
