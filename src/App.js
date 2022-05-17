35d8bf0aef4a7da7ad2ef7c8f08ef2361ee0de9d
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-05-17 15:39:38 +0500

    Revert back the initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Header from './components/Header';
 import './App.css';
+import CakeView from './pages/CakeView';
+import IcecreamView from './pages/IcecreamView';
+import UserView from './pages/UserView';
 
 function App() {
   return (
-    <div className="App">
-      Hello from App.js
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<CakeView />} />
+        <Route path="/icecream" element={<IcecreamView />} />
+        <Route path="/users" element={<UserView />} />
+      </Routes>
+    </Router>
   );
 }
 
