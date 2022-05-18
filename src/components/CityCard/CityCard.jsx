f033ab652fd90a7cfd1ea521492e9145c5de27f7
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-05-19 04:19:26 +0500

    Store Weather Data displayed through Routing

--- src/components/CityCard/CityCard.jsx
+++ src/components/CityCard/CityCard.jsx
@@ -1,9 +1,62 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useNavigate } from 'react-router-dom';
+import { fetchWeatherAction } from '../../redux/weather/weatherSlice';
+import cityData from '../City/cityData';
+import back from '../../assets/images/back.png';
 import './style.css';
 
 function CityCard() {
+  const { id } = useParams();
+  console.log(id);
+  const city = cityData[id];
+  const dispatch = useDispatch();
+  const history = useNavigate();
+  console.log(id);
+  console.log(city);
+
+  function handleGoBack(e) {
+    e.preventDefault();
+    history('/');
+  }
+
+  useEffect(() => {
+    dispatch(fetchWeatherAction(id));
+  }, []);
+
+  const state = useSelector((state) => state);
+  const { weather, loading, error } = state;
   return (
-    <div className="cityCard">
-      Helloooooooooooooooooooooooooooooooooooooooooo
+    <div>
+      <section className="relative bg-gray-900  min-h-screen">
+        {loading ? (
+          <h1 className="text-gray-400 text-4xl text-center">
+            Loading please wait...
+          </h1>
+        ) : error ? (
+          <h1 className="text-red-400 text-2xl text-center">
+            {error?.message}
+          </h1>
+        ) : (
+          <div className="p-8 border border-blue-800 rounded-lg">
+            <input className="go-back" type="image" src={back} onClick={handleGoBack} alt="go-back" />
+            <div className="flex justify-start  items-center">
+              <h2 className="text-gray-300 pl-5">{weather?.weather[0].main}</h2>
+              {' '}
+            </div>
+            <h2 className="mb-6 text-xl text-white font-semibold">
+              {weather?.name}
+              {', '}
+              {weather?.sys?.country}
+            </h2>
+            <h2 className="text-gray-300 text-center text-4xl mb-10">
+              {Math.ceil(Number(weather?.main.temp))}
+              {' '}
+              <span className="text-yellow-500 text-4xl">°C</span>
+            </h2>
+          </div>
+        )}
+      </section>
     </div>
   );
 }
--- src/redux/city/citySlice.js
+++ src/redux/city/citySlice.js
@@ -1,18 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
-  city: 'lahore',
+  loading: false,
+  cities: [],
+  error: '',
 };
+
+// Generates pending, fulfilled and rejected action types
+export const fetchCities = createAsyncThunk('city/fetchCities', () => axios
+  .get('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.data));
 
 const citySlice = createSlice({
   name: 'city',
   initialState,
-  reducers: {
-    selectedCity: (state, action) => {
+  extraReducers: (builder) => {
+    builder.addCase(fetchCities.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchCities.fulfilled, (state, action) => {
+      state.loading = false;
       state.city = action.payload;
-    },
+      state.error = '';
+    });
+    builder.addCase(fetchCities.rejected, (state, action) => {
+      state.loading = false;
+      state.city = [];
+      state.error = action.error.message;
+    });
   },
 });
 
 export default citySlice.reducer;
-export const { selectedCity } = citySlice.actions;
