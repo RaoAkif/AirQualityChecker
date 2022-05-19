cbdf9b1e383b6426e67f4e3dcc7f3fa7512c4323
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-05-19 22:48:24 +0500

    Add Tests

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
-// import {
-//   useSelector,
-//   useDispatch,
-// } from 'react-redux';
-// import { fetchCities } from '../../redux/city/citySlice';
-
 import City from '../../components/City/City';
 
 export default function Home() {
-  // const cities = useSelector((state) => state.cities);
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(fetchCities());
-  // }, []);
-
   return (
     <div className="home">
       <City />
