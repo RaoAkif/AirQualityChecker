2b4c90ddce2882e81348824845e1bdf003b229a7
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-05-20 01:48:53 +0500

    Remove ESLinter errors

--- src/pages/mock.test.js
+++ src/pages/mock.test.js
@@ -1,5 +1,5 @@
-jest.mock('./Home/Home.js');
-const main = require('./Home.js');
+jest.mock('./Home/Home');
+const main = require('./Home');
 
 describe('Home Page mocking', () => {
   it('Get the data from the store', () => {
